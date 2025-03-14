65894fd97c5f9d9be05cee5e3ea609912e266cf9
Dan Ko <danko56666@gmail.com>
2025-03-13 23:46:42 -0400

    chore: replace modal components

--- src/components/modals/GettingHelpModal.tsx
+++ src/components/modals/GettingHelpModal.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { useCallback } from "react";
-
 import Link from "next/link";
-import Modal from "@/components/modals/Modal";
-
+import { Modal } from "@chingu-x/components/modal";
 import { onCloseModal } from "@/store/features/modal/modalSlice";
 import { useAppDispatch, useModal } from "@/store/hooks";
 
