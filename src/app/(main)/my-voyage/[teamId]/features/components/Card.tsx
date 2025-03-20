0667e6905fdfe524e9818e8047abaab182d9e84b
Dan Ko <danko56666@gmail.com>
2025-03-20 05:00:24 -0400

    chore: remove feature service

--- src/app/(main)/my-voyage/[teamId]/features/components/Card.tsx
+++ src/app/(main)/my-voyage/[teamId]/features/components/Card.tsx
@@ -7,7 +7,6 @@ import {
   useRef,
   useState,
 } from "react";
-
 import Image from "next/image";
 import { Avatar } from "@chingu-x/components/avatar";
 import { type Feature } from "./fixtures/Features";
