9dcdeaef710eadb6633af43de5fee68d27d55b00
Dan Ko <danko56666@gmail.com>
2025-03-25 17:46:48 -0400

    feat: implement useFetchSprintsQuery

--- src/features/sprints/hooks/useFetchSprints.ts
+++ src/features/sprints/hooks/useFetchSprints.ts
@@ -1,9 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useFetchSprints } from "./useSprintsAdapters";
 import { useUserStateSelector } from "@/features/user/hooks/useUserStateSelector";
+import { useAppDispatch } from "@/shared/store";
+import { CacheTag } from "@/shared/utils/cacheTag";
+import { fetchSprintsState } from "@/features/sprints/store/sprintSlice";
 
-interface UseFetchSprintsProps {
+interface UseFetchSprintsQueryProps {
   teamId: string;
 }
 
-export async function useFetchSprintsProps({ teamId }: UseFetchSprintsProps) {
+export function useFetchSprintsQuery({ teamId }: UseFetchSprintsQueryProps) {
   const user = useUserStateSelector();
+  const dispatch = useAppDispatch();
+  const { fetchSprints } = useFetchSprints();
+
+  const {
+    isPending: isFetchSprintsPending,
+    isError: isFetchSprintsError,
+    error: fetchSprintsError,
+    data,
+  } = useQuery({
+    queryKey: [CacheTag.sprints, { teamId, user: `${user.id}` }],
+    queryFn: fetchSprintsQuery,
+  });
+
+  async function fetchSprintsQuery() {
+    return await fetchSprints({ teamId });
+  }
+
+  useEffect(() => {
+    if (data) {
+      dispatch(fetchSprintsState(data));
+    }
+  }, [data, dispatch]);
+
+  return {
+    isFetchSprintsPending,
+    isFetchSprintsError,
+    fetchSprintsError,
+  };
 }
