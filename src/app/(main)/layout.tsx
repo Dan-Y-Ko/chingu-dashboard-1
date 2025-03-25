bdc99a6bb7c0664478e20668e6e66fc6a4f343f2
Dan Ko <danko56666@gmail.com>
2025-03-25 17:38:28 -0400

    refactor: main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,24 +1,18 @@
 "use client";
 
 import "reflect-metadata";
-import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { Navbar } from "@chingu-x/components/navbar";
 import { Spinner } from "@chingu-x/components/spinner";
-import { useEffect } from "react";
 import ModeToggle from "@/shared/components/ModeToggle";
 import AuthHeader from "@/features/auth/components/AuthHeader";
 import { useAppDispatch } from "@/shared/store";
 import routePaths from "@/shared/utils/routePaths";
-import { CacheTag } from "@/shared/utils/cacheTag";
-import { currentDate } from "@/shared/utils/getCurrentDate";
 import ChinguMenu from "@/shared/components/navbar/ChinguMenu";
 import { onOpenModal } from "@/store/features/modal/modalSlice";
-import { setCurrentVoyageTeam } from "@/features/voyage-team/store/currentVoyageTeamSlice";
 import Sidebar from "@/shared/components/sidebar/Sidebar";
-import { useGetCurrentVoyageTeam } from "@/features/voyage-team/hooks/useVoyageTeamAdapters";
 import { useFetchUserQuery } from "@/features/user/hooks/useFetchUserQuery";
-import { sprintsAdapter } from "@/features/sprints/hooks/useSprintsAdapters";
+import { useFetchAllSprintsQuery } from "@/features/sprints/hooks/useFetchAllSprintsQuery";
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -27,9 +21,13 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   const dispatch = useAppDispatch();
   const router = useRouter();
-
   const { isFetchCurrentUserPending, isfetchCurrentUserError } =
     useFetchUserQuery();
+  const {
+    isFetchAllSprintsPending,
+    isfetchAllSprintsError,
+    fetchAllSprintsError,
+  } = useFetchAllSprintsQuery();
 
   if (isfetchCurrentUserError) {
     router.push(routePaths.signIn());
@@ -39,7 +37,7 @@ export default function Layout({ children }: LayoutProps) {
     dispatch(
       onOpenModal({
         type: "error",
-        content: { message: fetchAllSprintsError.message },
+        content: { message: fetchAllSprintsError!.message },
       }),
     );
   }
@@ -58,7 +56,7 @@ export default function Layout({ children }: LayoutProps) {
         <Sidebar />
         <main className="flex w-full flex-1 flex-col items-center overflow-y-auto p-10">
           <div className="flex w-full max-w-[1500px] flex-col gap-y-10">
-            {isFetchCurrentUserPending || fetchAllSprintsPending ? (
+            {isFetchCurrentUserPending || isFetchAllSprintsPending ? (
               <div className="flex min-h-screen items-center justify-center">
                 <Spinner />
               </div>
