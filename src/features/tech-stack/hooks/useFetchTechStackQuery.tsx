145fd644df2a087cb7bbfb357c6610cc30059570
Dan Ko <danko56666@gmail.com>
2025-04-07 23:02:53 -0400

    feat: implement useFetchTechStackQuery hook

---
+++ src/features/tech-stack/hooks/useFetchTechStackQuery.tsx
@@ -0,0 +1,52 @@
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { Spinner } from "@chingu-x/components/spinner";
+import { useFetchTechStack } from "./useTechStackAdapters";
+import { useAppDispatch } from "@/shared/store";
+import { CacheTag } from "@/shared/utils/cacheTag";
+import { fetchTechStackState } from "@/features/tech-stack/store/techStackSlice";
+import ErrorComponent from "@/shared/components/Error";
+import { ErrorType } from "@/shared/utils/error";
+
+interface UseFetchTechStackQueryProps {
+  teamId: string;
+}
+
+export function useFetchTechStackQuery({
+  teamId,
+}: UseFetchTechStackQueryProps) {
+  const dispatch = useAppDispatch();
+  const { fetchTechStack } = useFetchTechStack();
+
+  const { isPending, isError, error, data } = useQuery({
+    queryKey: [CacheTag.techStack, { teamId }],
+    queryFn: () => fetchTechStackFn(),
+  });
+
+  async function fetchTechStackFn() {
+    return await fetchTechStack({ teamId });
+  }
+
+  useEffect(() => {
+    if (data) {
+      dispatch(fetchTechStackState(data));
+    }
+  }, [data, dispatch]);
+
+  if (isError) {
+    return (
+      <ErrorComponent
+        errorType={ErrorType.FETCH_TECH_STACK}
+        message={error.message}
+      />
+    );
+  }
+
+  if (isPending) {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+}
--- src/features/tech-stack/hooks/useTechStackAdapters.ts
+++ src/features/tech-stack/hooks/useTechStackAdapters.ts
@@ -1,7 +1,17 @@
 import { resolve } from "@chingu-x/modules/resolver";
 import { TYPES } from "@chingu-x/modules/di-types";
-import type { TechStackClientAdapter } from "@chingu-x/modules/tech-stack";
+import type {
+  FetchTechStackClientRequestDto,
+  TechStackClientAdapter,
+} from "@chingu-x/modules/tech-stack";
 
 export const techStackAdapter = resolve<TechStackClientAdapter>(
   TYPES.TechStackClientAdapter,
 );
+
+export function useFetchTechStack() {
+  const fetchTechStack = async ({ teamId }: FetchTechStackClientRequestDto) =>
+    await techStackAdapter.fetchTechStack({ teamId });
+
+  return { fetchTechStack };
+}
