762f8c0b8cb8f429a7561d5709c16b55a6b02ff3
Dan Ko <danko56666@gmail.com>
2025-04-08 00:40:31 -0400

    refactor: RemoveVoteBtn component

--- src/features/tech-stack/components/RemoveVoteBtn.tsx
+++ src/features/tech-stack/components/RemoveVoteBtn.tsx
@@ -1,32 +1,17 @@
 import { Button } from "@chingu-x/components/button";
 import { Spinner } from "@chingu-x/components/spinner";
-import { removeTechItemVote } from "@/myVoyage/tech-stack/techStackService";
-import useServerAction from "@/shared/hooks/useServerAction";
-import { useAppDispatch } from "@/store/hooks";
-import { onOpenModal } from "@/store/features/modal/modalSlice";
+import { useRemoveTechStackVoteMutation } from "@/features/tech-stack/hooks/useRemoveTechStackVoteMutation";
 
 interface RemoveVoteBtnProps {
   techItemId: number;
 }
 
 export default function RemoveVoteBtn({ techItemId }: RemoveVoteBtnProps) {
-  const dispatch = useAppDispatch();
-  const {
-    runAction: removeVoteAction,
-    isLoading: removeVoteLoading,
-    setIsLoading: setRemoveVoteLoading,
-  } = useServerAction(removeTechItemVote);
+  const { isRemoveTechStackVotePending, removeTechStackVoteMutation } =
+    useRemoveTechStackVoteMutation();
 
-  const handleClick = async () => {
-    const [, error] = await removeVoteAction({
-      techItemId,
-    });
-    if (error) {
-      dispatch(
-        onOpenModal({ type: "error", content: { message: error.message } }),
-      );
-    }
-    setRemoveVoteLoading(false);
+  const handleClick = () => {
+    removeTechStackVoteMutation({ teamTechItemId: techItemId });
   };
 
   return (
@@ -34,12 +19,12 @@ export default function RemoveVoteBtn({ techItemId }: RemoveVoteBtnProps) {
       variant="error"
       size="xs"
       className={`justify-self-end rounded-3xl font-semibold ${
-        removeVoteLoading && "w-3/4"
+        isRemoveTechStackVotePending && "w-3/4"
       }`}
       onClick={handleClick}
-      disabled={removeVoteLoading}
+      disabled={isRemoveTechStackVotePending}
     >
-      {removeVoteLoading ? <Spinner /> : "Remove Vote"}
+      {isRemoveTechStackVotePending ? <Spinner /> : "Remove Vote"}
     </Button>
   );
 }
