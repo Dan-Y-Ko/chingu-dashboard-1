ffd2cc434655ac4c6204a637b3d5d7514846b8a7
Dan Ko <danko56666@gmail.com>
2025-04-07 23:08:35 -0400

    refactor tech stack page

--- src/app/(main)/my-voyage/[teamId]/tech-stack/page.tsx
+++ src/app/(main)/my-voyage/[teamId]/tech-stack/page.tsx
@@ -1,6 +1,12 @@
-import { Suspense } from "react";
-import { Spinner } from "@chingu-x/components/spinner";
-import TechStackComponentWrapper from "../../../../../features/tech-stack/components/TechStackComponentWrapper";
+"use client";
+
+import { Banner } from "@chingu-x/components/banner";
+import Image from "next/image";
+import { BannerContainer } from "@chingu-x/components/banner-container";
+import { useFetchTechStackQuery } from "@/features/tech-stack/hooks/useFetchTechStackQuery";
+import "reflect-metadata";
+import TechStackContainer from "@/features/tech-stack/components/TechStackContainer";
+import { useTechStackStateSelector } from "@/features/tech-stack/hooks/useTechStackStateSelector";
 
 interface TechStackPageProps {
   params: {
@@ -9,11 +15,42 @@ interface TechStackPageProps {
 }
 
 export default function TeckStackPage({ params }: TechStackPageProps) {
+  const { techStack } = useTechStackStateSelector();
+  const { teamId } = params;
+  useFetchTechStackQuery({ teamId });
+
   return (
     <>
-      <Suspense fallback={<Spinner />}>
-        <TechStackComponentWrapper params={params} />
-      </Suspense>
+      <BannerContainer
+        title="Tech Stack"
+        description="Alright, let's get down to business. We need to figure out which tech stack we're going to use to power this bad boy. Are you a JavaScript junkie, a Python pro, a Java genius, or a Ruby rockstar? Let's vote"
+      >
+        <Banner
+          imageLight={
+            <Image
+              src="/img/tech_stack_banner_light.png"
+              alt="Light tech stack banner"
+              fill={true}
+              sizes="276px"
+              priority
+              style={{ objectFit: "contain" }}
+            />
+          }
+          imageDark={
+            <Image
+              src="/img/tech_stack_banner_dark.png"
+              alt="Dark tech stack banner"
+              fill={true}
+              sizes="276px"
+              priority
+              style={{ objectFit: "contain" }}
+            />
+          }
+          height="h-[200px]"
+          width="w-[276px]"
+        />
+      </BannerContainer>
+      <TechStackContainer data={techStack} />
     </>
   );
 }
