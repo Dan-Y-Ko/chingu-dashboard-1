4a319eb41456c820c1c5b1af4b57656a3cf7baad
timDeHof <ttdehof@gmail.com>
2025-09-13 21:12:39 -0400

    refactor(DateTimePicker): ✏️ improve code formatting for consistency
    * Adjusted indentation for better readability.
    * Ensured consistent formatting across the component.

--- src/components/inputs/DateTimePicker.tsx
+++ src/components/inputs/DateTimePicker.tsx
@@ -9,54 +9,54 @@ import "react-datepicker/dist/react-datepicker.css";
 import TextInput from "./TextInput";
 
 export interface DatePickerInputProps {
-  id: string;
-  label: string;
-  selectedValue: Date;
-  placeholder: string;
-  errorMessage?: string | undefined;
-  className?: string;
-  onChange?: (date: Date | null) => void;
+    id: string;
+    label: string;
+    selectedValue: Date;
+    placeholder: string;
+    errorMessage?: string | undefined;
+    className?: string;
+    onChange?: (date: Date | null) => void;
 }
 
 function DateTimePicker({
-  id,
-  label,
-  selectedValue,
-  placeholder,
-  errorMessage,
-  className,
-  ...props
+    id,
+    label,
+    selectedValue,
+    placeholder,
+    errorMessage,
+    className,
+    ...props
 }: DatePickerInputProps) {
     const filterPassedTime = (time: Date) => {
-      const currentDate = new Date();
-      const selectedDate = new Date(time);
-      return currentDate.getTime() < selectedDate.getTime();
+        const currentDate = new Date();
+        const selectedDate = new Date(time);
+        return currentDate.getTime() < selectedDate.getTime();
     };
     return (
-      <div className="flex w-full flex-col">
-        <ReactDatePicker
-          id={id}
-          selected={selectedValue}
-          placeholderText={placeholder}
-          showTimeSelect
-          filterTime={filterPassedTime}
-          dateFormat="MMMM d, yyyy h:mm aa"
-          timeIntervals={15}
-          popperClassName="ml-1"
-          popperPlacement="bottom-start"
-          customInput={
-            <TextInput
-              id={id}
-              label={label}
-              placeholder=""
-              inputGroupContent={<CalendarDaysIcon />}
-              errorMessage={errorMessage}
-              className={className}
+        <div className="flex w-full flex-col">
+            <ReactDatePicker
+                id={id}
+                selected={selectedValue}
+                placeholderText={placeholder}
+                showTimeSelect
+                filterTime={filterPassedTime}
+                dateFormat="MMMM d, yyyy h:mm aa"
+                timeIntervals={15}
+                popperClassName="ml-1"
+                popperPlacement="bottom-start"
+                customInput={
+                    <TextInput
+                        id={id}
+                        label={label}
+                        placeholder=""
+                        inputGroupContent={<CalendarDaysIcon />}
+                        errorMessage={errorMessage}
+                        className={className}
+                    />
+                }
+                {...props}
             />
-          }
-          {...props}
-        />
-      </div>
+        </div>
     );
 }
 
