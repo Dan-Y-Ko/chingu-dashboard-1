ff775dc04af9593dd3d1b28ecfb2eb42c6abb93d
timDeHof <ttdehof@gmail.com>
2025-09-13 21:21:52 -0400

    refactor(jest.config): ✏️ remove unnecessary `await` from `createJestConfig`
    * Simplified the configuration by removing `await` as `createJestConfig` is not an async function.

--- jest.config.mjs
+++ jest.config.mjs
@@ -25,7 +25,7 @@ const clientTestConfig = {
 
 const config = {
   // Add more setup options before each test is run
-  projects: [await createJestConfig(clientTestConfig)()],
+  projects: [createJestConfig(clientTestConfig)()],
 };
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default config;
