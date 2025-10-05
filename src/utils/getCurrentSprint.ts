2fbacf29208ba0a99105adebce1017bb4ccf2458
timDeHof <ttdehof@gmail.com>
2025-10-04 22:44:18 -0400

    refactor(getCurrentSprint): ♻️ remove unused `currentDate` variable
    * Commented out the `currentDate` variable that was previously used for development.
    * Updated the `getCurrentSprint` function to use a local `currentDate` variable instead.

--- src/utils/getCurrentSprint.ts
+++ src/utils/getCurrentSprint.ts
@@ -2,10 +2,12 @@ import { isWithinInterval } from "date-fns";
 
 import { type Sprint } from "@/store/features/sprint/sprintSlice";
 
-export const currentDate =
-  process.env.NODE_ENV === "development" ? new Date(2024, 11, 3) : new Date();
+// export const currentDate =
+//   process.env.NODE_ENV === "development" ? new Date(2024, 11, 3) : new Date();
 
-export function getCurrentSprint(sprints: Sprint[]) {
+export function getCurrentSprint(sprints: Sprint[], mockDate?: Date) {
+  const currentDate = mockDate || new Date();
+
   const currentSprint = sprints.find((sprint) =>
     isWithinInterval(currentDate, {
       start: sprint.startDate,
