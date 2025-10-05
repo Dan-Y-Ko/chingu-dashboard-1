e7bd96fb506e0690fab0197c8fa90a42e473ad82
timDeHof <ttdehof@gmail.com>
2025-10-04 22:44:43 -0400

    refactor(getCurrentSprint): ♻️ improve code formatting for consistency
    * Adjust indentation for better readability.
    * Ensure consistent formatting in the `getCurrentSprint` function.

--- src/utils/getCurrentSprint.ts
+++ src/utils/getCurrentSprint.ts
@@ -6,14 +6,14 @@ import { type Sprint } from "@/store/features/sprint/sprintSlice";
 //   process.env.NODE_ENV === "development" ? new Date(2024, 11, 3) : new Date();
 
 export function getCurrentSprint(sprints: Sprint[], mockDate?: Date) {
-  const currentDate = mockDate || new Date();
+    const currentDate = mockDate || new Date();
 
-  const currentSprint = sprints.find((sprint) =>
-    isWithinInterval(currentDate, {
-      start: sprint.startDate,
-      end: sprint.endDate,
-    }),
-  );
+    const currentSprint = sprints.find((sprint) =>
+        isWithinInterval(currentDate, {
+            start: sprint.startDate,
+            end: sprint.endDate,
+        }),
+    );
 
-  return currentSprint;
+    return currentSprint;
 }
